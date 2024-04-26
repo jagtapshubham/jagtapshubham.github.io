f8b27d39eed11c567f6fc1ecfbfe9b7daa53646f
jagtapshubham <jagtapshubham190@gmail.com>
2024-04-26 19:33:18 +0530

    migrated to tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        darkblue: "rgb(17, 9, 34)",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
