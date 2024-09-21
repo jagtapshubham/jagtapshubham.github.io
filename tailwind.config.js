c51b25e7660e192a31e29a4697e07ef8f39a936c
jagtapshubham <jagtapshubham190@gmail.com>
2024-09-21 20:33:39 +0530

    - Added introduction section - Hi there and waving hand added - copywrite added in footer - tailwindcss and sql skill added

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,24 @@ module.exports = {
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
       },
+      keyframes: {
+        wave: {
+          "0%": { transform: "rotate(0.0deg)" },
+          "10%": { transform: "rotate(14deg)" },
+          "20%": { transform: "rotate(-8deg)" },
+          "30%": { transform: "rotate(14deg)" },
+          "40%": { transform: "rotate(-4deg)" },
+          "50%": { transform: "rotate(10.0deg)" },
+          "60%": { transform: "rotate(0.0deg)" },
+          "100%": { transform: "rotate(0.0deg)" },
+        },
+      },
+      animation: {
+        "waving-hand": "wave 2s linear infinite",
+      },
+      backgroundImage: {
+        "star-pattern": "url('/images/stars.png)",
+      },
     },
   },
   plugins: [],
