5f87a4917ce7ec513da62d00d6134cd0c1b0bd65
jagtapshubham <jagtapshubham190@gmail.com>
2024-10-17 00:56:30 +0530

    fix: updated experience section and resume

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 /* FrontEnd List */
-const searsDevExperience = document.getElementById("SearsFrontEnd");
-const devListExperience = searsDevExperience.nextElementSibling;
+// const searsDevExperience = document.getElementById("SearsFrontEnd");
+// const devListExperience = searsDevExperience.nextElementSibling;
 
-const toggleDevExperience = () => {
-  devListExperience.classList.toggle("hidden");
-  searsDevExperience.lastElementChild.classList.toggle("rotate-180");
-};
-searsDevExperience.addEventListener("click", toggleDevExperience);
+// const toggleDevExperience = () => {
+//   devListExperience.classList.toggle("hidden");
+//   searsDevExperience.lastElementChild.classList.toggle("rotate-180");
+// };
+// searsDevExperience.addEventListener("click", toggleDevExperience);
 
 /* QA List */
-const searsQaExperience = document.getElementById("SearsQa");
-const qaListExperience = searsQaExperience.nextElementSibling;
+// const searsQaExperience = document.getElementById("SearsQa");
+// const qaListExperience = searsQaExperience.nextElementSibling;
 
-const toggleQaExperience = () => {
-  qaListExperience.classList.toggle("hidden");
-  searsQaExperience.lastElementChild.classList.toggle("rotate-180");
-};
-searsQaExperience.addEventListener("click", toggleQaExperience);
+// const toggleQaExperience = () => {
+//   qaListExperience.classList.toggle("hidden");
+//   searsQaExperience.lastElementChild.classList.toggle("rotate-180");
+// };
+// searsQaExperience.addEventListener("click", toggleQaExperience);
