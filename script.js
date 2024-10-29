41393c1b2990f1290b8f3bcd6b882c92099e1ee9
jagtapshubham <jagtapshubham190@gmail.com>
2024-10-29 18:07:11 +0530

    fix: removed # from url, updated resume, smooth scroll

--- script.js
+++ script.js
@@ -1,19 +1,36 @@
 /* FrontEnd List */
-// const searsDevExperience = document.getElementById("SearsFrontEnd");
-// const devListExperience = searsDevExperience.nextElementSibling;
+const searsDevExperience = document.getElementById("SearsFrontEnd");
+const devListExperience = searsDevExperience.nextElementSibling;
 
-// const toggleDevExperience = () => {
-//   devListExperience.classList.toggle("hidden");
-//   searsDevExperience.lastElementChild.classList.toggle("rotate-180");
-// };
-// searsDevExperience.addEventListener("click", toggleDevExperience);
+const toggleDevExperience = () => {
+  devListExperience.classList.toggle("hidden");
+  searsDevExperience.lastElementChild.classList.toggle("rotate-180");
+};
+searsDevExperience.addEventListener("click", toggleDevExperience);
 
 /* QA List */
-// const searsQaExperience = document.getElementById("SearsQa");
-// const qaListExperience = searsQaExperience.nextElementSibling;
+const searsQaExperience = document.getElementById("SearsQa");
+const qaListExperience = searsQaExperience.nextElementSibling;
 
-// const toggleQaExperience = () => {
-//   qaListExperience.classList.toggle("hidden");
-//   searsQaExperience.lastElementChild.classList.toggle("rotate-180");
-// };
-// searsQaExperience.addEventListener("click", toggleQaExperience);
+const toggleQaExperience = () => {
+  qaListExperience.classList.toggle("hidden");
+  searsQaExperience.lastElementChild.classList.toggle("rotate-180");
+};
+searsQaExperience.addEventListener("click", toggleQaExperience);
+
+/* Removing # from url when nav links are clicked */
+document.querySelectorAll("nav a").forEach((link) => {
+  link.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    const targetId = event.target.getAttribute("data-scroll-to");
+    const targetSection = document.getElementById(targetId);
+
+    if (targetSection) {
+      targetSection.scrollIntoView({ behavior: "smooth" });
+      targetId === "home"
+        ? history.pushState(null, null, `/`)
+        : history.pushState(null, null, `/${targetId}`);
+    }
+  });
+});
